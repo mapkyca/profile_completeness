1b246a0ca1be6e6860284799760f216072318dde
Marcus Povey <marcus@marcus-povey.co.uk>
2012-10-10 16:50:27 +0100

    Removing obsolete dir.

--- language/en.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $english = array(
-		'profilecomplete:title' => 'Profile Completeness',
-        'profilecomplete' => 'Your profile is %d%% complete.',
-        'profilecomplete:widget:description' => 'Display the completeness of the profile.',
-    );
-
-    add_translation("en",$english);
