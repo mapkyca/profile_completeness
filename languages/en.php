716e73e1a5d68057586f6db446a00f6436e21f11
Marcus Povey <marcus@marcus-povey.co.uk>
2012-10-10 12:37:42 +0100

    Language>Languages

---
+++ languages/en.php
@@ -0,0 +1,9 @@
+<?php
+
+    $english = array(
+		'profilecomplete:title' => 'Profile Completeness',
+        'profilecomplete' => 'Your profile is %d%% complete.',
+        'profilecomplete:widget:description' => 'Display the completeness of the profile.',
+    );
+
+    add_translation("en",$english);
